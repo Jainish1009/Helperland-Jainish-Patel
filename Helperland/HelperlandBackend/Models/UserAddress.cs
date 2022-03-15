3c03ac13e1e67f5ec96c6f387588d8c35e9d5dd3
Jainish patel <jainishpatel1009@gmail.com>
2022-03-15 10:21:01 +0530

    Adding service provider

---
+++ Helperland/HelperlandBackend/Models/UserAddress.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace helperland1._0.Models
+{
+    public partial class UserAddress
+    {
+        public int AddressId { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string AddressLine1 { get; set; }
+        [Required]
+        public string AddressLine2 { get; set; }
+        [Required]
+        public string City { get; set; }
+        public string State { get; set; }
+        [Required]
+        [StringLength(6, ErrorMessage = "Please Enter Valid Postal Code", MinimumLength = 6)]
+        public string PostalCode { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsDeleted { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Please Enter Valid Phone No")]
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
